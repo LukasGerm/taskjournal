cb5a643f04b3354714d0a648b00b50d8e938a47d
LukasGerm <lukas@lukasgermerott.de>
2023-07-28 21:31:55 +0200

    feat: add in firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:taskjournal/pages/todo.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const Taskjournal());
